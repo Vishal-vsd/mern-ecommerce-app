0861303519c976ca1a7289006f953ce56db79d34
Vishal Dhiman <vishaldhimann27@gmail.com>
2026-05-07 16:46:14 +0530

    created register and login routes as auth routes and tested in postman and it is working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const cookieParser = require("cookie-parser")
+const authRoutes = require("./routes/authRoutes.js")
 
 const connectDB = require("./config/db.js")
 
@@ -9,13 +11,16 @@ const app = express();
 connectDB();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("API running...")
 })
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
